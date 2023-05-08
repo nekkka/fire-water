c19acfecfb4f228b687ead684bf3df6e6e9fe501
Nelle <nurievaaa28@gmail.com>
2023-05-08 22:34:35 +0600

    ffff

--- Desktop/nfac/fire_water/3.py
+++ Desktop/nfac/fire_water/3.py
@@ -2,102 +2,195 @@ import pygame
 import os
 import random
 
-os.chdir('C:/Users/ASUS/Desktop/pp2/fire_water')
+os.chdir('C:/Users/ASUS/Desktop/nfac/fire_water') 
 pygame.init()
 width = 1200
 height = 700
 
+pygame.init()
 screen = pygame.display.set_mode((width, height))
 pygame.display.set_caption("Fire and water")
 
 
 class Fire(pygame.sprite.Sprite):
-    def __init__(self, width, height):
-        super().__init__()
+    def __init__(self, width, height, blocks):
+        super().__init__() 
         self.image = pygame.image.load("fire.png")
         self.image = pygame.transform.scale(self.image, (40, 80))
         self.rect = self.image.get_rect()
         self.rect.center = (50, 620)
         self.width = width
         self.height = height
-        self.jump_height = 10
+        self.jump_height = 11
         self.jump_velocity = 0
         self.gravity = 0.5
         self.is_jumping = False
-
+        self.blocks = blocks  # добавить список блоков
+ 
     def update(self):
         pressed_keys = pygame.key.get_pressed()
 
         if self.is_jumping:
-            self.rect.move_ip(0, -self.jump_velocity)
+            if pressed_keys[pygame.K_LEFT] and self.rect.left > 30:
+                self.rect.move_ip(-5, -self.jump_velocity)
+            elif pressed_keys[pygame.K_RIGHT] and self.rect.right < self.width - 30:
+                self.rect.move_ip(5, -self.jump_velocity)
+            else:
+                self.rect.move_ip(0, -self.jump_velocity)
             self.jump_velocity -= self.gravity
             if self.rect.bottom >= self.height:
                 self.is_jumping = False
                 self.jump_velocity = 0
         else:
-            if self.rect.left > 30:
-                if pressed_keys[pygame.K_LEFT]:
-                    self.rect.move_ip(-5, 0)
-            if self.rect.right < self.width - 30:
-                if pressed_keys[pygame.K_RIGHT]:
-                    self.rect.move_ip(5, 0)
-            if self.rect.top > 0:
-                if pressed_keys[pygame.K_UP] and not self.is_jumping:
-                    self.is_jumping = True
-                    self.jump_velocity = self.jump_height
+            if self.rect.left > 30 and pressed_keys[pygame.K_LEFT]:
+                self.rect.move_ip(-5, 0)
+            if self.rect.right < self.width - 30 and pressed_keys[pygame.K_RIGHT]:
+                self.rect.move_ip(5, 0)
+            if self.rect.top > 0 and pressed_keys[pygame.K_UP] and not self.is_jumping:
+                self.is_jumping = True
+                self.jump_velocity = self.jump_height
 
+        # добавить проверку столкновения с блоками
+        for block in self.blocks:
+            if block.rect.colliderect(self.rect.x + 5, self.rect.y, self.rect.width, self.rect.height):
+                self.rect.move_ip(-5, 0)
+            if block.rect.colliderect(self.rect.x - 5, self.rect.y, self.rect.width, self.rect.height):
+                self.rect.move_ip(5, 0)
+            if block.rect.colliderect(self.rect.x, self.rect.y + 5, self.rect.width, self.rect.height):
+                self.is_jumping = False
+                self.rect.move_ip(0, -5)
+                self.jump_velocity = 0
+            if block.rect.colliderect(self.rect.x, self.rect.y - 5, self.rect.width, self.rect.height):
+                self.rect.move_ip(0, 5)
+                self.jump_velocity = 0
+
+
+            # for block in self.blocks:
+            #     if hasattr(block, "rect") and block.rect.colliderect(self.rect.x + 5, self.rect.y, self.rect.width, self.rect.height):
+            #         self.rect.move_ip(-5, 0)
+            #     if hasattr(block, "rect") and block.rect.colliderect(self.rect.x - 5, self.rect.y, self.rect.width, self.rect.height):
+            #         self.rect.move_ip(5, 0)
+            #     if hasattr(block, "rect") and block.rect.colliderect(self.rect.x, self.rect.y + 5, self.rect.width, self.rect.height):
+            #         self.is_jumping = False
+            #         self.rect.move_ip(0, -5)
+            #         self.jump_velocity = 0
+            #     if hasattr(block, "rect") and block.rect.colliderect(self.rect.x, self.rect.y - 5, self.rect.width, self.rect.height):
+            #         self.rect.move_ip(0, 5)
+            #         self.jump_velocity = 0
+ 
     def draw(self, surface):
         surface.blit(self.image, self.rect)
 
 
+
+
 class Water(pygame.sprite.Sprite):
-    def __init__(self, width, height):
-        super().__init__()
+    def __init__(self, width, height, blocks):
+        super().__init__() 
         self.image = pygame.image.load("water.png")
         self.image = pygame.transform.scale(self.image, (40, 80))
         self.rect = self.image.get_rect()
         self.rect.center = (50, 620)
         self.width = width
         self.height = height
-        self.jump_height = 10
+        self.jump_height = 11
         self.jump_velocity = 0
         self.gravity = 0.5
         self.is_jumping = False
-
+        self.blocks = blocks  # добавить список блоков
+ 
     def update(self):
         pressed_keys = pygame.key.get_pressed()
 
         if self.is_jumping:
-            self.rect.move_ip(0, -self.jump_velocity)
+            if pressed_keys[pygame.K_a] and self.rect.left > 30:
+                self.rect.move_ip(-5, -self.jump_velocity)
+            elif pressed_keys[pygame.K_d] and self.rect.right < self.width - 30:
+                self.rect.move_ip(5, -self.jump_velocity)
+            else:
+                self.rect.move_ip(0, -self.jump_velocity)
             self.jump_velocity -= self.gravity
             if self.rect.bottom >= self.height:
                 self.is_jumping = False
                 self.jump_velocity = 0
         else:
-            if self.rect.left > 30:
-                if pressed_keys[pygame.K_a]:
-                    self.rect.move_ip(-5, 0)
-            if self.rect.right < self.width - 30:
-                if pressed_keys[pygame.K_d]:
-                    self.rect.move_ip(5, 0)
-            if self.rect.top > 0:
-                if pressed_keys[pygame.K_w] and not self.is_jumping:
-                    self.is_jumping = True
-                    self.jump_velocity = self.jump_height
+            if self.rect.left > 30 and pressed_keys[pygame.K_a]:
+                self.rect.move_ip(-5, 0)
+            if self.rect.right < self.width - 30 and pressed_keys[pygame.K_d]:
+                self.rect.move_ip(5, 0)
+            if self.rect.top > 0 and pressed_keys[pygame.K_w] and not self.is_jumping:
+                self.is_jumping = True
+                self.jump_velocity = self.jump_height
 
+        # добавить проверку столкновения с блоками
+        for block in self.blocks:
+            if block.rect.colliderect(self.rect.x + 5, self.rect.y, self.rect.width, self.rect.height):
+                self.rect.move_ip(-5, 0)
+            if block.rect.colliderect(self.rect.x - 5, self.rect.y, self.rect.width, self.rect.height):
+                self.rect.move_ip(5, 0)
+            if block.rect.colliderect(self.rect.x, self.rect.y + 5, self.rect.width, self.rect.height):
+                self.is_jumping = False
+                self.rect.move_ip(0, -5)
+                self.jump_velocity = 0
+            if block.rect.colliderect(self.rect.x, self.rect.y - 5, self.rect.width, self.rect.height):
+                self.rect.move_ip(0, 5)
+                self.jump_velocity = 0
+ 
     def draw(self, surface):
         surface.blit(self.image, self.rect)
 
 
+
 class Block:
     def __init__(self, _x=0, _y=0, image_path="block.png"):
+        super().__init__()
         self.x = _x
         self.y = _y
         self.image = pygame.image.load(image_path).convert_alpha()
-        self.image = pygame.transform.scale(self.image, (10, 10))
+        self.image = pygame.transform.scale(self.image, (40, 40))
+        self.rect = self.image.get_rect()
+        self.rect.x = self.x
+        self.rect.y = self.y
 
     def draw(self):
         screen.blit(self.image, (self.x, self.y))
 
 
-def own_round(value,
+prepatsv = [Block(x, y) for x in range(25, 500, 40) for y in range(530, 570, 40)]
+pol = [Block(x, y) for x in range(0, width, 40) for y in range(height-50, height, 40)]
+blocks=[]
+blocks.extend(prepatsv)
+blocks.extend(pol)
+
+
+fire_sprite = Fire(width, height, blocks)
+water_sprite = Water(width, height, blocks)
+
+
+# blocks = [Block(x, y) for x in range(25, 500, 50) for y in range(500, 550, 50)]
+
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+
+    oblozhka = pygame.image.load('bg.png')
+    oblozhka = pygame.transform.scale(oblozhka, (1200, 700))
+    screen.blit(oblozhka, (0 , 0))
+
+    fire_sprite.update()
+    water_sprite.update()
+    for block in blocks:
+            block.draw()
+    for i in prepatsv:
+            i.draw()
+
+    fire_sprite.draw(screen)
+    water_sprite.draw(screen)
+    pygame.display.flip()
+
+
+
+pygame.quit()
--- Desktop/nfac/fire_water/try.py
+++ Desktop/nfac/fire_water/try.py
@@ -2,69 +2,79 @@ import pygame
 import os
 import random
 
-os.chdir('C:/Users/ASUS/Desktop/nfac/fire_water') 
-pygame.init()
-width = 1200
-height = 700
+# Set the working directory to the folder containing the game files
+os.chdir('C:/Users/ASUS/Desktop/nfac/fire_water')
 
+# Initialize pygame
 pygame.init()
-screen = pygame.display.set_mode((width, height))
-pygame.display.set_caption("Fire and water")
 
+# Set the screen dimensions
+SCREEN_WIDTH = 1200
+SCREEN_HEIGHT = 700
+
+# Set the game window caption
+pygame.display.set_caption("Fire and Water")
+
+# Create the game screen
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
 class Fire(pygame.sprite.Sprite):
-    def __init__(self, width, height, blocks):
-        super().__init__() 
-        self.image = pygame.image.load("fire.png")
+    def __init__(self, blocks):
+        super().__init__()
+        # Load the image for the sprite
+        self.image = pygame.image.load("fire.png").convert_alpha()
         self.image = pygame.transform.scale(self.image, (40, 80))
+        # Set the initial position of the sprite
         self.rect = self.image.get_rect()
         self.rect.center = (50, 620)
-        self.width = width
-        self.height = height
+        # Set some physics properties for the sprite
         self.jump_height = 10
         self.jump_velocity = 0
         self.gravity = 0.5
         self.is_jumping = False
+        # Store a reference to the blocks in the level
         self.blocks = blocks
  
     def update(self):
+        # Check which keys are being pressed
         pressed_keys = pygame.key.get_pressed()
 
+        # Update the position of the sprite based on its current state
         if self.is_jumping:
             self.rect.move_ip(0, -self.jump_velocity)
             self.jump_velocity -= self.gravity
-            if self.rect.bottom >= self.height:
+            if self.rect.bottom >= SCREEN_HEIGHT:
                 self.is_jumping = False
                 self.jump_velocity = 0
         else:
             if self.rect.left > 30:
                 if pressed_keys[pygame.K_LEFT]:
                     self.rect.move_ip(-5, 0)
-            if self.rect.right < self.width - 30:        
+            if self.rect.right < SCREEN_WIDTH - 30:        
                 if pressed_keys[pygame.K_RIGHT]:
                     self.rect.move_ip(5, 0)
-
-            # check collision with blocks
-            for block in self.blocks:
-                if self.rect.colliderect(block.rect):
-                    if self.rect.bottom <= block.rect.top + 10:
-                        # land on block
-                        self.rect.bottom = block.rect.top
-                        self.is_jumping = False
-                        self.jump_velocity = 0
-                        break
-                    else:
-                        # hit block
-                        self.rect.left = max(self.rect.left, block.rect.right)
-                        self.rect.right = min(self.rect.right, block.rect.left)
-                        break
-
             if self.rect.top > 0:
                 if pressed_keys[pygame.K_UP] and not self.is_jumping:
                     self.is_jumping = True
                     self.jump_velocity = self.jump_height
 
+        # Check for collisions with blocks in the level
+        for block in self.blocks:
+            if self.rect.colliderect(block.rect):
+                if self.rect.bottom <= block.rect.top + 10:
+                    # Landed on the block
+                    self.rect.bottom = block.rect.top
+                    self.is_jumping = False
+                    self.jump_velocity = 0
+                    break
+                elif self.rect.top >= block.rect.bottom - 10:
+                    # Hit the block from above
+                    self.rect.top = block.rect.bottom
+                    self.jump_velocity = 0
+                    break
+
     def draw(self, surface):
+        # Draw the sprite on the given surface
         surface.blit(self.image, self.rect)
 
 
@@ -107,6 +117,7 @@ class Water(pygame.sprite.Sprite):
     def draw(self, surface):
         surface.blit(self.image, self.rect)
 
+
 class Block:
     def __init__(self, _x=0, _y=0, image_path="block.png"):
         super().__init__()
@@ -118,24 +129,25 @@ class Block:
     def draw(self):
         screen.blit(self.image, (self.x, self.y))
 
-
 blocks = [Block(x, y) for x in range(25, 500, 50) for y in range(500, 550, 50)]
-fire_sprite = Fire(width, height, blocks)
-water_sprite = Water(width, height)
+# Create the sprites and blocks
+fire_sprite = Fire(50, 50)
+water_sprite = Water(50, 50)
 
-# blocks = [Block(x, y) for x in range(25, 500, 50) for y in range(500, 550, 50)]
 
+# Start the game loop
 running = True
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
-
+    # Draw the background
     oblozhka = pygame.image.load('bg.png')
     oblozhka = pygame.transform.scale(oblozhka, (1200, 700))
     screen.blit(oblozhka, (0 , 0))
 
+    # Update and draw the sprites and blocks
     fire_sprite.update()
     water_sprite.update()
     for block in blocks:
@@ -145,6 +157,5 @@ while running:
     water_sprite.draw(screen)
     pygame.display.flip()
 
-
-
+# Quit the game
 pygame.quit()
